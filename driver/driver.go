70d13228c5735aeb7843d3cc6a15ac23d3ef3680
Chris Burkert <christian.burkert@sap.com>
2018-06-15 11:29:14 +0200

    add connection.Exec

--- driver/driver.go
+++ driver/driver.go
@@ -158,6 +158,11 @@ func (connection *connection) Begin() (driver.Tx, error) {
 	return &transaction{conn: connection.conn}, nil
 }
 
+func (connection *connection) Exec(query string, args []driver.Value) (driver.Result, error) {
+	// TODO
+	return nil, nil
+}
+
 func (connection *connection) Query(query string, args []driver.Value) (driver.Rows, error) {
 	// TODO
 	return nil, nil
