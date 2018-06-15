8d713913741f895ba1e6223e3352e5e8908556b9
Chris Burkert <christian.burkert@sap.com>
2018-06-15 09:40:00 +0200

    add initial driver.go

---
+++ driver/driver.go
@@ -0,0 +1,12 @@
+package driver
+
+/*
+#cgo CFLAGS: -I${SRCDIR}/includes
+#cgo LDFLAGS: -Wl,-rpath,\$ORIGIN
+#include <stdlib.h>
+#include "ctpublic.h"
+*/
+import "C"
+
+//DriverName is the driver name to use with sql.Open for ase databases.
+const DriverName = "ase"
