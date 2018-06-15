5e177dfa1b8f127fc8790ef59927bdf45198b84e
Chris Burkert <christian.burkert@sap.com>
2018-06-15 09:35:11 +0200

    add header files

---
+++ driver/includes/sqlca.h
@@ -0,0 +1,38 @@
+/*
+**	SAP OC-LIBRARY
+**	Copyright (c) 2013 SAP AG or an SAP affiliate company.  All rights reserved.
+*/
+
+/*
+** sqlca.h - This is the header file for the sqlca structure for precompilers
+*/
+#ifndef __SQLCA_H__
+
+#define __SQLCA_H__
+
+/*****************************************************************************
+**
+** sqlca structure used
+**
+*****************************************************************************/
+
+typedef struct _sqlca
+{
+	char	sqlcaid[8];
+	long	sqlcabc;
+	long	sqlcode;
+	
+	struct
+	{
+		long		sqlerrml;
+		char		sqlerrmc[256];
+	} sqlerrm;
+
+	char	sqlerrp[8];
+	long	sqlerrd[6];
+	char	sqlwarn[8];
+	char	sqlext[8];
+
+} SQLCA;
+
+#endif /* __SQLCA_H__ */
---
+++ driver/includes/sqlda.h
@@ -0,0 +1,44 @@
+/*
+**		SAP CT-LIBRARY
+**		Copyright (c) 2013 SAP AG or an SAP affiliate company.  All rights reserved.
+*/
+
+#ifndef __SQLDA_H__
+#define __SQLDA_H__
+
+typedef struct _sqlda
+{
+	CS_SMALLINT sd_sqln;
+	CS_SMALLINT sd_sqld;
+	struct _sd_column
+	{
+		CS_DATAFMT sd_datafmt;
+		CS_VOID *sd_sqldata;
+		CS_SMALLINT sd_sqlind;
+		CS_INT sd_sqllen;
+		CS_VOID	*sd_sqlmore;
+	} sd_column[1];
+} syb_sqlda;
+
+typedef syb_sqlda SQLDA;
+
+#define SYB_SQLDA_SIZE(n) (sizeof(SQLDA) \
+		- sizeof(struct _sd_column) \
+		+ (n) * sizeof(struct _sd_column))
+
+#ifndef SQLDA_DECL
+#define SQLDA_DECL(name, size) \
+struct { \
+	CS_SMALLINT sd_sqln; \
+	CS_SMALLINT sd_sqld; \
+	struct { \
+		CS_DATAFMT sd_datafmt; \
+		CS_VOID *sd_sqldata; \
+		CS_SMALLINT sd_sqlind; \
+		CS_INT sd_sqllen; \
+		CS_VOID	*sd_sqlmore; \
+	} sd_column[(size)]; \
+} name
+#endif /* SQLDA_DECL */
+
+#endif /* __SQLDA_H__ */
