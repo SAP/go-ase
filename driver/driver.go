313c3338307888dfe02e7a8ec2674e6253e72d63
Chris Burkert <christian.burkert@sap.com>
2018-06-15 10:34:45 +0200

    initialize context

--- driver/driver.go
+++ driver/driver.go
@@ -7,6 +7,11 @@ package driver
 #include "ctpublic.h"
 */
 import "C"
+import (
+	"database/sql"
+	"fmt"
+	"unsafe"
+)
 
 //DriverName is the driver name to use with sql.Open for ase databases.
 const DriverName = "ase"
@@ -16,3 +21,20 @@ type drv struct{}
 var (
 	cContext *C.CS_CONTEXT
 )
+
+func init() {
+	sql.Register(DriverName, &drv{})
+	rc := C.cs_ctx_alloc(C.CS_CURRENT_VERSION, &cContext)
+	if rc != C.CS_SUCCEED {
+		fmt.Println("C.cs_ctx_alloc failed")
+		return
+	}
+	defer C.free(unsafe.Pointer(cContext))
+
+	rc = C.ct_init(cContext, C.CS_CURRENT_VERSION)
+	if rc != C.CS_SUCCEED {
+		fmt.Println("C.ct_init failed")
+		C.cs_ctx_drop(cContext)
+		return
+	}
+}
