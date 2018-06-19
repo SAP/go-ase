fb8a5f68cfc381a92c1d9a99d8149bbc9e25284a
Chris Burkert <christian.burkert@sap.com>
2018-06-19 17:03:59 +0200

    use flags in the example code

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,32 @@ package main
 
 import (
 	"database/sql"
+	"flag"
 	"fmt"
 
+	"github.com/bgentry/speakeasy"
 	_ "github.wdf.sap.corp/bssdb/go-ase/driver"
 )
 
+var (
+	fHost = flag.String("H", "localhost", "database hostname")
+	fPort = flag.String("P", "4901", "database sql port")
+	fUser = flag.String("u", "sa", "database user name")
+	fPass = flag.String("p", "", "database user password")
+)
+
 func main() {
-	db, err := sql.Open("ase", "ase://user:pass@host:port")
+	flag.Parse()
+	pass := *fPass
+	var err error
+	if len(pass) == 0 {
+		pass, err = speakeasy.Ask("Please enter the password of user " + *fUser + ": ")
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	db, err := sql.Open("ase", "ase://"+*fUser+":"+pass+"@"+*fHost+":"+*fPort)
 	if err != nil {
 		fmt.Println(err)
 		return
