781c324e40c701ed2b46da03ec5c1254e28a8577
Nelo-T. Wallus <nelo@wallus.de>
2018-11-28 14:19:57 +0100

    Move *Msg functions to bridge.go

---
+++ driver/bridge.go
@@ -0,0 +1,54 @@
+package driver
+
+// #include "ctpublic.h"
+import "C"
+import (
+	"fmt"
+	"os"
+	"strings"
+	"unsafe"
+)
+
+// srvMsg is a callback function which will be called from C when the server sends a message.
+// This may be an error message or e.g. the information that we are connected successfully.
+// Don't change the following line. It is the directive for cgo to make the function available from C.
+//export srvMsg
+func srvMsg(msg *C.CS_SERVERMSG) {
+	switch msg.msgnumber {
+	case 8589940293:
+		fmt.Printf("Successfully connected with %s\n", strings.Trim(C.GoString((*C.char)(unsafe.Pointer(&msg.text))), "\n"))
+	default:
+		fmt.Fprintln(os.Stderr, "Server message:")
+		fmt.Fprintf(os.Stderr, "msgnumber:   %d\n", msg.msgnumber)
+		fmt.Fprintf(os.Stderr, "state:       %d\n", msg.state)
+		fmt.Fprintf(os.Stderr, "severity:    %d\n", msg.severity)
+		fmt.Fprintf(os.Stderr, "text:        %s\n", C.GoString((*C.char)(unsafe.Pointer(&msg.text))))
+		fmt.Fprintf(os.Stderr, "textlen:     %d\n", msg.textlen)
+		fmt.Fprintf(os.Stderr, "server:      %s\n", C.GoString((*C.char)(unsafe.Pointer(&msg.svrname))))
+		fmt.Fprintf(os.Stderr, "svrnlen:     %d\n", msg.svrnlen)
+		fmt.Fprintf(os.Stderr, "proc:        %s\n", C.GoString((*C.char)(unsafe.Pointer(&msg.proc))))
+		fmt.Fprintf(os.Stderr, "proclen:     %d\n", msg.proclen)
+		fmt.Fprintf(os.Stderr, "line:        %d\n", msg.line)
+		fmt.Fprintf(os.Stderr, "status:      %d\n", msg.status)
+		fmt.Fprintf(os.Stderr, "sqlstate:    %s\n", C.GoString((*C.char)(unsafe.Pointer(&msg.sqlstate))))
+		fmt.Fprintf(os.Stderr, "sqlstatelen: %d\n", msg.sqlstatelen)
+	}
+}
+
+// ctlMsg is a callback function which will be called from C when the client sends a message.
+// This may be an error message or some information.
+// Don't change the following line. It is the directive for cgo to make the function available from C.
+//export ctlMsg
+func ctlMsg(msg *C.CS_CLIENTMSG) {
+	fmt.Fprintln(os.Stderr, "Client message:")
+	fmt.Fprintf(os.Stderr, "severity:     %d\n", msg.severity)
+	fmt.Fprintf(os.Stderr, "msgnumber:    %d\n", msg.msgnumber)
+	fmt.Fprintf(os.Stderr, "msgstring:    %s\n", C.GoString((*C.char)(unsafe.Pointer(&msg.msgstring))))
+	fmt.Fprintf(os.Stderr, "msgstringlen: %d\n", msg.msgstringlen)
+	fmt.Fprintf(os.Stderr, "osnumber:     %d\n", msg.osnumber)
+	fmt.Fprintf(os.Stderr, "osstring:     %s\n", C.GoString((*C.char)(unsafe.Pointer(&msg.osstring))))
+	fmt.Fprintf(os.Stderr, "osstringlen:  %d\n", msg.osstringlen)
+	fmt.Fprintf(os.Stderr, "status:       %d\n", msg.status)
+	fmt.Fprintf(os.Stderr, "sqlstate:     %s\n", C.GoString((*C.char)(unsafe.Pointer(&msg.sqlstate))))
+	fmt.Fprintf(os.Stderr, "sqlstatelen:  %d\n", msg.sqlstatelen)
+}
