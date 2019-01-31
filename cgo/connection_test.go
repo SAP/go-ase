df1756614b6248b46db85465c12ed1f6db2c6e4c
Nelo-T. Wallus <nelo@wallus.de>
2019-01-31 10:37:56 +0100

    Add TestPing{,Err}

--- cgo/connection_test.go
+++ cgo/connection_test.go
@@ -1,6 +1,10 @@
 package cgo
 
-import "testing"
+import (
+	"context"
+	"database/sql/driver"
+	"testing"
+)
 
 func TestNewConnection(t *testing.T) {
 	conn, err := newConnection(*testDsn)
@@ -11,5 +15,35 @@ func TestNewConnection(t *testing.T) {
 	err = conn.Close()
 	if err != nil {
 		t.Errorf("Unexpected error closing connection: %v", err)
+	}
+}
+
+func TestPing(t *testing.T) {
+	conn, err := newConnection(*testDsn)
+	if err != nil {
+		t.Errorf("Unexpected error opening connection: %v", err)
+	}
+	defer conn.Close()
+
+	err = conn.Ping(context.Background())
+	if err != nil {
+		t.Errorf("Got error pinging ASE: %v", err)
+	}
+}
+
+func TestPingErr(t *testing.T) {
+	conn, err := newConnection(*testDsn)
+	if err != nil {
+		t.Errorf("Unexpected error opening connection: %v", err)
+	}
+
+	err = conn.Close()
+	if err != nil {
+		t.Errorf("Unexpected error closing connection: %v", err)
+	}
+
+	err = conn.Ping(context.Background())
+	if err != driver.ErrBadConn {
+		t.Errorf("Did not receive driver.ErrBadConn: %v", err)
 	}
 }
