d48c34c1908b739d6976f37388e85f52395922b1
Nelo-T. Wallus <nelo@wallus.de>
2019-02-08 07:51:10 +0100

    Fix server/client callbacks causing not returning expected values

--- cgo/bridge.go
+++ cgo/bridge.go
@@ -12,7 +12,7 @@ import (
 // This may be an error message or e.g. the information that we are connected successfully.
 // Don't change the following line. It is the directive for cgo to make the function available from C.
 //export srvMsg
-func srvMsg(msg *C.CS_SERVERMSG) {
+func srvMsg(msg *C.CS_SERVERMSG) C.CS_RETCODE {
 	switch msg.msgnumber {
 	case C.CS_SV_INFORM:
 		break
@@ -32,13 +32,15 @@ func srvMsg(msg *C.CS_SERVERMSG) {
 		fmt.Fprintf(os.Stderr, "sqlstate:    %s\n", C.GoString((*C.char)(unsafe.Pointer(&msg.sqlstate))))
 		fmt.Fprintf(os.Stderr, "sqlstatelen: %d\n", msg.sqlstatelen)
 	}
+
+	return C.CS_SUCCEED
 }
 
 // ctlMsg is a callback function which will be called from C when the client sends a message.
 // This may be an error message or some information.
 // Don't change the following line. It is the directive for cgo to make the function available from C.
 //export ctlMsg
-func ctlMsg(msg *C.CS_CLIENTMSG) {
+func ctlMsg(msg *C.CS_CLIENTMSG) C.CS_RETCODE {
 	fmt.Fprintln(os.Stderr, "Client message:")
 	fmt.Fprintf(os.Stderr, "severity:     %d\n", msg.severity)
 	fmt.Fprintf(os.Stderr, "msgnumber:    %d\n", msg.msgnumber)
@@ -50,4 +52,6 @@ func ctlMsg(msg *C.CS_CLIENTMSG) {
 	fmt.Fprintf(os.Stderr, "status:       %d\n", msg.status)
 	fmt.Fprintf(os.Stderr, "sqlstate:     %s\n", C.GoString((*C.char)(unsafe.Pointer(&msg.sqlstate))))
 	fmt.Fprintf(os.Stderr, "sqlstatelen:  %d\n", msg.sqlstatelen)
+
+	return C.CS_SUCCEED
 }
