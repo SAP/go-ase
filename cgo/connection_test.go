63fe1c2a50ac02170e2578aaa6fff55930909faa
Nelo-T. Wallus <nelo@wallus.de>
2019-02-14 14:57:29 +0100

    Add cgo.TestExec

--- cgo/connection_test.go
+++ cgo/connection_test.go
@@ -3,6 +3,8 @@ package cgo
 import (
 	"context"
 	"database/sql/driver"
+	"math/rand"
+	"strconv"
 	"testing"
 )
 
@@ -45,5 +47,36 @@ func TestPingErr(t *testing.T) {
 	err = conn.Ping(context.Background())
 	if err != driver.ErrBadConn {
 		t.Errorf("Did not receive driver.ErrBadConn: %v", err)
+	}
+}
+
+func TestExec(t *testing.T) {
+	tableName := "table" + strconv.Itoa(rand.Int())
+
+	conn, err := newConnection(*testDsn)
+	if err != nil {
+		t.Errorf("Unexpected error opening connection: %v", err)
+		return
+	}
+	defer conn.Close()
+	result, err := conn.Exec("create table "+tableName+" (a int, b char(3))", nil)
+
+	if err != nil {
+		t.Errorf("Received error when creating table: %v", err)
+		return
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		t.Errorf("Received error when reading affected rows: %v", err)
+	} else {
+		if rowsAffected != -1 {
+			t.Errorf("Result indicated affected rows when there should be none: %d", rowsAffected)
+		}
+	}
+
+	result, err = conn.Exec("drop table "+tableName, nil)
+	if err != nil {
+		t.Errorf("Received error when dropping table: %v", err)
 	}
 }
