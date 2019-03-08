5d5c6eff3f2d5cf5cca4234d96688f121eb918be
Nelo-T. Wallus <nelo@wallus.de>
2019-03-08 10:59:23 +0100

    Implement driver.Tx

--- cgo/transaction.go
+++ cgo/transaction.go
@@ -12,6 +12,14 @@ import (
 // transaction is the struct which represents a database transaction.
 type transaction struct {
 	conn *connection
+	// ASE does not support read-only transactions - the connection
+	// itself however can be set as read-only.
+	// readonlyPreTx signals if a connection was marked as read-only
+	// before the transaction startet.
+	readonlyPreTx bool
+	// readonlyNeedsReset is true if the read-only option passed to
+	// BeginTx differs from the read-only property of the connection.
+	readonlyNeedsReset bool
 }
 
 // Interface satisfaction checks
@@ -26,32 +34,74 @@ func (conn *connection) BeginTx(ctx context.Context, opts driver.TxOptions) (dri
 		return nil, fmt.Errorf("Unsupported isolation level requested: %d", opts.Isolation)
 	}
 
-	rc := C.ct_options(conn.conn, C.CS_SET, C.CS_OPT_ISOLATION, unsafe.Pointer(&opts.Isolation), C.CS_UNUSED, nil)
+	tx := &transaction{conn, false, false}
+
+	_, err := tx.conn.Exec("BEGIN TRANSACTION", nil)
+	if err != nil {
+		return fmt.Errorf("Failed to start transaction: %v", err)
+	}
+
+	_, err := tx.conn.Exec("SET TRANSACTION ISOLATION LEVEL ?", []driver.Value{opts.Isolation})
+	if err != nil {
+		return fmt.Errorf("Failed to set isolation level for transaction: %v", err)
+	}
+
+	ro := C.CS_FALSE
+	rc = C.ct_con_preops(tx.conn.conn, C.CS_GET, C.CS_PROP_READONLY, unsafe.Pointer(&ro), C.CS_UNUSED, nil)
 	if rc != C.CS_SUCCEED {
-		return nil, makeError(rc, "Failed to set isolation")
+		return nil, makeError(rc, "Failed to retrieve readonly property")
 	}
 
-	readOnly := C.CS_FALSE
-	if opts.ReadOnly {
-		readOnly = C.CS_TRUE
+	if opts.Readonly != bool(ro) {
+		tx.readonlyPreTx = bool(ro)
+		tx.readonlyNeedsReset = true
+
+		err = tx.setRO(opts.ReadOnly)
+		if err != nil {
+			return nil, err
+		}
 	}
 
-	rc = C.ct_con_props(conn.conn, C.CS_SET, C.CS_PROP_READONLY, unsafe.Pointer(&readOnly), C.CS_UNUSED, nil)
+	return tx, nil
+}
+
+func (tx *transaction) Commit() error {
+	_, err := tx.conn.Exec("COMMIT TRANSACTION", nil)
+	if err != nil {
+		return err
+	}
+	return tx.finish()
+}
+
+func (tx *transaction) Rollback() error {
+	_, err := tx.conn.Exec("ROLLBACK TRANSACTION", nil)
+	if err != nil {
+		return err
+	}
+	return tx.finish()
+}
+
+func (tx *transaction) finish() error {
+	if tx.readonlyNeedsReset {
+		err := tx.setRO(tx.roPreTx)
+		if err != nil {
+			return err
+		}
+	}
+	tx.conn = nil
+	return nil
+}
+
+func (tx *transaction) setRO(ro bool) error {
+	ro := C.CS_FALSE
+	if ro {
+		ro = C.CS_TRUE
+	}
+
+	rc = C.ct_con_props(tx.conn.conn, C.CS_SET, C.CS_PROP_READONLY, unsafe.Pointer(&ro), C.CS_UNUSED, nil)
 	if rc != C.CS_SUCCEED {
 		return nil, makeError(rc, "Failed to set readonly")
 	}
 
-	// TODO disable autocommit
-
-	return &transaction{conn: conn}, nil
-}
-
-func (transaction *transaction) Commit() error {
-	// TODO
-	return nil
-}
-
-func (transaction *transaction) Rollback() error {
-	// TODO
 	return nil
 }
