615b216892a18ad87b01a9010b7fb22baeb9017c
Nelo-T. Wallus <nelo@wallus.de>
2019-03-29 09:56:22 +0100

    Add tests for all supported data types

---
+++ tests/libtest/type_float.go
@@ -0,0 +1,48 @@
+package libtest
+
+import (
+	"database/sql"
+	"math"
+	"testing"
+)
+
+func DoTestFloat64(t *testing.T) {
+	TestForEachDB("TestFloat64", t, testFloat64)
+}
+
+func testFloat64(t *testing.T, db *sql.DB, tableName string) {
+	samples := []float64{math.SmallestNonzeroFloat64, math.MaxFloat64}
+
+	pass := make([]interface{}, len(samples))
+	for i, sample := range samples {
+		pass[i] = sample
+	}
+	rows, err := SetupTableInsert(db, tableName, "float", pass...)
+	if err != nil {
+		t.Errorf("%v", err)
+		return
+	}
+	defer rows.Close()
+
+	i := 0
+	var recv float64
+	for rows.Next() {
+		err = rows.Scan(&recv)
+		if err != nil {
+			t.Errorf("Scan failed on %dth scan: %v", i, err)
+			continue
+		}
+
+		if recv != samples[i] {
+			t.Errorf("Received value does not match passed parameter")
+			t.Errorf("Expected: %v", samples[i])
+			t.Errorf("Received: %v", recv)
+		}
+
+		i += 1
+	}
+
+	if err := rows.Err(); err != nil {
+		t.Errorf("Error preparing rows: %v", err)
+	}
+}
---
+++ tests/libtest/type_time.go
@@ -0,0 +1,55 @@
+package libtest
+
+import (
+	"database/sql"
+	"testing"
+	"time"
+)
+
+func DoTestTime(t *testing.T) {
+	TestForEachDB("TestTime", t, testTime)
+}
+
+func testTime(t *testing.T, db *sql.DB, tableName string) {
+	samples := []time.Time{
+		// Sybase & Golang zero-value; January 1, 0001 Midnight
+		time.Time{},
+		time.Date(2019, time.March, 29, 9, 26, 0, 0, time.UTC),
+		// Sybase max
+		time.Date(9999, time.December, 31, 23, 59, 59, 999999000, time.UTC),
+	}
+
+	pass := make([]interface{}, len(samples))
+	for i, sample := range samples {
+		pass[i] = sample
+	}
+
+	rows, err := SetupTableInsert(db, tableName, "bigdatetime", pass...)
+	if err != nil {
+		t.Errorf("%v", err)
+		return
+	}
+	defer rows.Close()
+
+	i := 0
+	var recv time.Time
+	for rows.Next() {
+		err = rows.Scan(&recv)
+		if err != nil {
+			t.Errorf("Scan failed on %dth scan: %v", i, err)
+			continue
+		}
+
+		if recv != samples[i] {
+			t.Errorf("Received value does not match passed parameter")
+			t.Errorf("Expected: %v", samples[i])
+			t.Errorf("Received: %v", recv)
+		}
+
+		i += 1
+	}
+
+	if err := rows.Err(); err != nil {
+		t.Errorf("Error preparing rows: %v", err)
+	}
+}
