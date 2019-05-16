77d823b13f20add7c84e284a4e1fa405626178f1
Nelo-T. Wallus <nelo@wallus.de>
2019-05-16 10:53:56 +0200

    Support DSN flags

--- cmd/cgo-isql/main.go
+++ cmd/cgo-isql/main.go
@@ -13,11 +13,42 @@ import (
 )
 
 var (
+	fHost         = flag.String("H", "", "database hostname")
+	fPort         = flag.String("P", "", "database sql port")
+	fUser         = flag.String("u", "", "database user name")
+	fPass         = flag.String("p", "", "database user password")
+	fUserstorekey = flag.String("k", "", "userstorekey")
+	fDatabase     = flag.String("D", "", "database")
+
 	fOpts = &flagslice.FlagStringSlice{}
 )
 
 func openDB() (*ase.Connection, error) {
 	dsn := libdsn.NewDsnInfoFromEnv("")
+
+	if *fHost != "" {
+		dsn.Host = *fHost
+	}
+
+	if *fPort != "" {
+		dsn.Port = *fPort
+	}
+
+	if *fUser != "" {
+		dsn.Username = *fUser
+	}
+
+	if *fPass != "" {
+		dsn.Password = *fPass
+	}
+
+	if *fUserstorekey != "" {
+		dsn.Userstorekey = *fUserstorekey
+	}
+
+	if *fDatabase != "" {
+		dsn.Database = *fDatabase
+	}
 
 	for _, fOpt := range fOpts.Slice() {
 		split := strings.SplitN(fOpt, "=", 2)
