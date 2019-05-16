142bad5d0dd608de00af623843a32802d9a69d97
Nelo-T. Wallus <nelo@wallus.de>
2019-05-16 11:17:38 +0200

    Add Example test for example/cgo/recorder

--- examples/cgo/recorder/main.go
+++ examples/cgo/recorder/main.go
@@ -8,7 +8,7 @@ import (
 	"log"
 
 	"github.com/SAP/go-ase/cgo"
-	libdsn "github.com/SAP/go-ase/libase/dsn"
+	"github.com/SAP/go-ase/libase/libdsn"
 )
 
 func main() {
@@ -28,16 +28,38 @@ func DoMain() error {
 	}
 	defer db.Close()
 
+	// Execute a ping here - the connection through database/sql will
+	// only be created once a query is performed.
+	// This causes the server to send messages regarding the context
+	// switches, which we do not want to test for.
+	// To prevent the context switch messages being recorded a query is
+	// performed before attaching the recorder to the message broker.
+	err = db.Ping()
+	if err != nil {
+		return fmt.Errorf("Failed to ping database")
+	}
+
 	fmt.Println("Creating MessageRecorder")
 	recorder := cgo.NewMessageRecorder()
 	fmt.Println("Registering handler with server message broker")
 	cgo.GlobalServerMessageBroker.RegisterHandler(recorder.HandleMessage)
 
-	fmt.Println("Calling dbcc")
-	// TODO: dbcc command with persistent output
-	_, err = db.Exec("dbcc checkalloc")
+	fmt.Println("Enable traceflag 3604")
+	_, err = db.Exec("dbcc traceon(3604)")
 	if err != nil {
-		return err
+		return fmt.Errorf("Failed to enable traceflag 3604")
+	}
+
+	fmt.Println("Received messages:")
+	for _, line := range recorder.Text() {
+		fmt.Print(line)
+	}
+
+	fmt.Println("Listing enabled traceflags")
+	recorder.Reset()
+	_, err = db.Exec("dbcc traceflags")
+	if err != nil {
+		return fmt.Errorf("Failed to list traceflags")
 	}
 
 	fmt.Println("Received messages:")
---
+++ examples/cgo/recorder/main_test.go
@@ -0,0 +1,22 @@
+package main
+
+import "log"
+
+func ExampleDoMain() {
+	err := DoMain()
+	if err != nil {
+		log.Printf("Failed to execute example: %v", err)
+	}
+	// Output:
+	// Opening database
+	// Creating MessageRecorder
+	// Registering handler with server message broker
+	// Enable traceflag 3604
+	// Received messages:
+	// DBCC execution completed. If DBCC printed error messages, contact a user with System Administrator (SA) role.
+	// Listing enabled traceflags
+	// Received messages:
+	// Active traceflags: 3604
+	//
+	// DBCC execution completed. If DBCC printed error messages, contact a user with System Administrator (SA) role.
+}
