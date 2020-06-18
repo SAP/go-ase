2c1f44895433aa1ab0f369e5c916574cd8730e9f
Nelo-T. Wallus <nelo@wallus.de>
2020-06-18 11:15:36 +0000

    Generate proper errors for ErrorPackage

--- libase/tds/packageError.go
+++ libase/tds/packageError.go
@@ -20,46 +20,50 @@ func (pkg *ErrorPackage) ReadFrom(ch *channel) error {
 	var err error
 	pkg.Length, err = ch.Uint16()
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to read length: %w", err)
 	}
 
 	pkg.ErrorNumber, err = ch.Int32()
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to read error number: %w", err)
 	}
 
 	pkg.MsgLength, err = ch.Uint16()
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to read message length: %w", err)
 	}
 
 	pkg.ErrorMsg, err = ch.String(int(pkg.MsgLength))
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to read error message: %w", err)
 	}
 
 	pkg.ServerLength, err = ch.Uint8()
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to read servername length: %w", err)
 	}
 
 	pkg.ServerName, err = ch.String(int(pkg.ServerLength))
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to read servername: %w", err)
 	}
 
 	pkg.ProcLength, err = ch.Uint8()
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to read procname length: %w", err)
 	}
 
 	pkg.ProcName, err = ch.String(int(pkg.ProcLength))
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to read procname: %w", err)
 	}
 
 	pkg.LineNr, err = ch.Uint16()
-	return err
+	if err != nil {
+		return fmt.Errorf("failed to read line nr: %w", err)
+	}
+
+	return nil
 }
 
 func (pkg ErrorPackage) WriteTo(ch *channel) error {
