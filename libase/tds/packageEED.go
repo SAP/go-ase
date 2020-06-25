4d5c6a6ed8558c84bf269011ce4b948c35e0047d
Nelo-T. Wallus <nelo@wallus.de>
2020-06-25 11:50:33 +0000

    Add descriptive errors in EEDPackage

--- libase/tds/packageEED.go
+++ libase/tds/packageEED.go
@@ -28,82 +28,86 @@ func (pkg *EEDPackage) ReadFrom(ch *channel) error {
 	var err error
 	pkg.Length, err = ch.Uint16()
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to read TDS token %s: %w", TDS_EED, err)
 	}
 
 	pkg.MsgNumber, err = ch.Uint32()
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to read message number: %w", err)
 	}
 
 	pkg.State, err = ch.Uint8()
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to read state: %w", err)
 	}
 
 	pkg.Class, err = ch.Uint8()
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to read class: %w", err)
 	}
 
 	var sqlStateLen uint8
 	sqlStateLen, err = ch.Uint8()
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to read SQL state length: %w", err)
 	}
 
 	pkg.SQLState, err = ch.Bytes(int(sqlStateLen))
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to read SQL state: %w", err)
 	}
 
 	var status uint8
 	status, err = ch.Uint8()
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to read status: %w", err)
 	}
 	pkg.Status = EEDStatus(status)
 
 	pkg.TranState, err = ch.Uint16()
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to read tran state: %w", err)
 	}
 
 	var msgLength uint16
 	msgLength, err = ch.Uint16()
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to read message length: %w", err)
 	}
 
 	pkg.Msg, err = ch.String(int(msgLength))
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to read message: %w", err)
 	}
 
 	var serverLength uint8
 	serverLength, err = ch.Uint8()
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to read server name length: %w", err)
 	}
 
 	pkg.ServerName, err = ch.String(int(serverLength))
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to read server name: %w", err)
 	}
 
 	var procLength uint8
 	procLength, err = ch.Uint8()
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to read proc name length: %w", err)
 	}
 
 	pkg.ProcName, err = ch.String(int(procLength))
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to read proc name: %w", err)
 	}
 
 	pkg.LineNr, err = ch.Uint16()
-	return err
+	if err != nil {
+		return fmt.Errorf("failed to read line nr: %w", err)
+	}
+
+	return nil
 }
 
 func (pkg EEDPackage) WriteTo(ch *channel) error {
@@ -132,6 +136,11 @@ func (pkg EEDPackage) WriteTo(ch *channel) error {
 		return fmt.Errorf("failed to write class: %w", err)
 	}
 
+	err = ch.WriteUint8(uint8(len(pkg.SQLState)))
+	if err != nil {
+		return fmt.Errorf("failed to write SQL state len: %w", err)
+	}
+
 	err = ch.WriteBytes(pkg.SQLState)
 	if err != nil {
 		return fmt.Errorf("failed to write SQL state: %w", err)
@@ -147,14 +156,29 @@ func (pkg EEDPackage) WriteTo(ch *channel) error {
 		return fmt.Errorf("failed to write tran state: %w", err)
 	}
 
+	err = ch.WriteUint16(uint16(len(pkg.Msg)))
+	if err != nil {
+		return fmt.Errorf("failed to write message length: %w", err)
+	}
+
 	err = ch.WriteString(pkg.Msg)
 	if err != nil {
 		return fmt.Errorf("failed to write message: %w", err)
 	}
 
+	err = ch.WriteUint8(uint8(len(pkg.ServerName)))
+	if err != nil {
+		return fmt.Errorf("failed to write server name length: %w", err)
+	}
+
 	err = ch.WriteString(pkg.ServerName)
 	if err != nil {
 		return fmt.Errorf("failed to write server name: %w", err)
+	}
+
+	err = ch.WriteUint8(uint8(len(pkg.ProcName)))
+	if err != nil {
+		return fmt.Errorf("failed to write proc name length: %w", err)
 	}
 
 	err = ch.WriteString(pkg.ProcName)
