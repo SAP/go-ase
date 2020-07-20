3dc06eed29beaceab891a69d0caf4ffa13a55e27
Nelo-T. Wallus <nelo@wallus.de>
2020-07-20 11:50:52 +0000

    Add HeaderOnlyPackage

---
+++ libase/tds/packageHeaderOnly.go
@@ -0,0 +1,22 @@
+package tds
+
+import "fmt"
+
+// HeaderOnlyPackage is used to communicate header-only packets using
+// the same cummincation channels as regular token-based packages in
+// go-ase.
+type HeaderOnlyPackage struct {
+	Header MessageHeader
+}
+
+func (pkg HeaderOnlyPackage) ReadFrom(ch BytesChannel) error {
+	return fmt.Errorf("HeaderOnlyPackages cannot be read from a ByteChannel")
+}
+
+func (pkg HeaderOnlyPackage) WriteTo(ch BytesChannel) error {
+	return fmt.Errorf("HeaderOnlyPackages cannot be written to a ByteChannel")
+}
+
+func (pkg HeaderOnlyPackage) String() string {
+	return fmt.Sprintf("Header: %s", pkg.Header)
+}
