0e47374d34a3b5f3ef287648d7c99a222d579a92
Nelo-T. Wallus <nelo@wallus.de>
2020-07-21 09:17:54 +0000

    update goconntest

--- cmd/goconntest/main.go
+++ cmd/goconntest/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 
 	"github.com/SAP/go-ase/libase/libdsn"
@@ -14,29 +13,24 @@ func main() {
 	dsn := libdsn.NewDsnInfoFromEnv("")
 
 	log.Printf("Dialing to server")
-	tdsConn, err := tds.NewTDSConn(
-		context.Background(),
-		"tcp",
-		fmt.Sprintf("%s:%s", dsn.Host, dsn.Port),
-	)
+	conn, err := tds.NewConn(context.Background(), dsn)
 	if err != nil {
 		log.Printf("Failed to dial server: %v", err)
 		return
 	}
 	defer func() {
-		err := tdsConn.Close()
+		err := conn.Close()
 		if err != nil {
 			log.Printf("Error closing connection: %v", err)
 		}
 	}()
 
 	// Channel 0
-	tdsCh0, err := tdsConn.NewTDSChannel(100)
+	ch0, err := conn.NewChannel()
 	if err != nil {
 		log.Printf("Failed to open channel: %w", err)
 		return
 	}
-	defer tdsCh0.Close()
 
 	log.Printf("Preparing login packet")
 	conf, err := tds.NewLoginConfig(dsn)
@@ -47,7 +41,7 @@ func main() {
 	conf.AppName = "goconntest"
 
 	log.Printf("Logging in")
-	err = tdsCh0.Login(conf)
+	err = ch0.Login(context.Background(), conf)
 	if err != nil {
 		log.Printf("Login failed: %v", err)
 		return
@@ -55,27 +49,34 @@ func main() {
 
 	langPkg := &tds.LanguagePackage{
 		Status: tds.TDS_LANGUAGE_NOARGS,
-		Cmd:    "sp_help",
+		Cmd:    "select 'ping'",
 	}
 
 	log.Printf("Sending language command")
-	err = tdsCh0.QueuePackage(langPkg)
+	err = ch0.SendPackage(context.Background(), langPkg)
 	if err != nil {
 		log.Printf("Error sending package: %w", err)
 		return
 	}
 
-	err = tdsCh0.SendRemainingPackets()
-	if err != nil {
-		log.Printf("Error sending packets: %w", err)
-		return
-	}
-
-	pkg, err := tdsCh0.NextPackage(true)
+	// rowfmt
+	_, err = ch0.NextPackage(true)
 	if err != nil {
 		log.Printf("Error receiving package: %w", err)
 		return
 	}
 
-	log.Printf("%#v", pkg)
+	// row
+	_, err = ch0.NextPackage(true)
+	if err != nil {
+		log.Printf("Error receiving package: %w", err)
+		return
+	}
+
+	// done
+	_, err = ch0.NextPackage(true)
+	if err != nil {
+		log.Printf("Error receiving package: %w", err)
+		return
+	}
 }
