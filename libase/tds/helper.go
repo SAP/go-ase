eb30e5df39b4f92cc5a2a685a4899cd47e647e2e
Nelo-T. Wallus <nelo@wallus.de>
2020-07-21 09:09:18 +0000

    Return error in writeString if padding is impossible

--- libase/tds/helper.go
+++ libase/tds/helper.go
@@ -1,11 +1,17 @@
 package tds
 
 import (
+	"fmt"
 	"io"
 )
 
 // writeString writes s padded to padTo and its length to buf.
 func writeString(stream io.Writer, s string, padTo int) error {
+	if len(s) > padTo {
+		return fmt.Errorf("string '%s' is too large, must be at most %d bytes long",
+			s, padTo)
+	}
+
 	_, err := stream.Write([]byte(s))
 	if err != nil {
 		return err
