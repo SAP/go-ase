e16a5a4ab51b4d0880999a5efcb40eeb1d1eb6cb
Nelo-T. Wallus <nelo@wallus.de>
2020-08-12 10:38:36 +0000

    Validate written data length and expected data length

--- libase/tds/packageError.go
+++ libase/tds/packageError.go
@@ -3,22 +3,17 @@ package tds
 import "fmt"
 
 type ErrorPackage struct {
-	Length       uint16
-	ErrorNumber  int32
-	State        uint8
-	Class        uint8
-	MsgLength    uint16
-	ErrorMsg     string
-	ServerLength uint8
-	ServerName   string
-	ProcLength   uint8
-	ProcName     string
-	LineNr       uint16
+	ErrorNumber int32
+	State       uint8
+	Class       uint8
+	ErrorMsg    string
+	ServerName  string
+	ProcName    string
+	LineNr      uint16
 }
 
 func (pkg *ErrorPackage) ReadFrom(ch BytesChannel) error {
-	var err error
-	pkg.Length, err = ch.Uint16()
+	expectLength, err := ch.Uint16()
 	if err != nil {
 		return ErrNotEnoughBytes
 	}
@@ -27,40 +22,53 @@ func (pkg *ErrorPackage) ReadFrom(ch BytesChannel) error {
 	if err != nil {
 		return ErrNotEnoughBytes
 	}
+	n := 4
 
-	pkg.MsgLength, err = ch.Uint16()
+	msgLength, err := ch.Uint16()
 	if err != nil {
 		return ErrNotEnoughBytes
 	}
+	n += 2
 
-	pkg.ErrorMsg, err = ch.String(int(pkg.MsgLength))
+	pkg.ErrorMsg, err = ch.String(int(msgLength))
 	if err != nil {
 		return ErrNotEnoughBytes
 	}
+	n += int(msgLength)
 
-	pkg.ServerLength, err = ch.Uint8()
+	serverLength, err := ch.Uint8()
 	if err != nil {
 		return ErrNotEnoughBytes
 	}
+	n++
 
-	pkg.ServerName, err = ch.String(int(pkg.ServerLength))
+	pkg.ServerName, err = ch.String(int(serverLength))
 	if err != nil {
 		return ErrNotEnoughBytes
 	}
+	n += int(serverLength)
 
-	pkg.ProcLength, err = ch.Uint8()
+	procLength, err := ch.Uint8()
 	if err != nil {
 		return ErrNotEnoughBytes
 	}
+	n++
 
-	pkg.ProcName, err = ch.String(int(pkg.ProcLength))
+	pkg.ProcName, err = ch.String(int(procLength))
 	if err != nil {
 		return ErrNotEnoughBytes
 	}
+	n += int(procLength)
 
 	pkg.LineNr, err = ch.Uint16()
 	if err != nil {
 		return ErrNotEnoughBytes
+	}
+	n += 2
+
+	if n != int(expectLength) {
+		return fmt.Errorf("expected to read %d bytes, read %d bytes instead",
+			expectLength, n)
 	}
 
 	return nil
@@ -72,7 +80,19 @@ func (pkg ErrorPackage) WriteTo(ch BytesChannel) error {
 		return fmt.Errorf("failed to write TDS Token %s: %w", TDS_ERROR, err)
 	}
 
-	err = ch.WriteUint16(pkg.Length)
+	// 4 errornumber
+	// 1 state
+	// 1 class
+	// 2 len(errormsg)
+	// x errormsg
+	// 1 len(servername)
+	// x servername
+	// 1 len(procname)
+	// x procname
+	// 2 linenr
+	expectLength := 12 + len(pkg.ErrorMsg) + len(pkg.ServerName) + len(pkg.ProcName)
+
+	err = ch.WriteUint16(uint16(expectLength))
 	if err != nil {
 		return fmt.Errorf("failed to write length: %w", err)
 	}
@@ -81,50 +101,65 @@ func (pkg ErrorPackage) WriteTo(ch BytesChannel) error {
 	if err != nil {
 		return fmt.Errorf("failed to write error number: %w", err)
 	}
+	n := 4
 
 	err = ch.WriteUint8(pkg.State)
 	if err != nil {
 		return fmt.Errorf("failed to write state: %w", err)
 	}
+	n++
 
 	err = ch.WriteUint8(pkg.Class)
 	if err != nil {
 		return fmt.Errorf("failed to write class: %w", err)
 	}
+	n++
 
-	err = ch.WriteUint16(pkg.MsgLength)
+	err = ch.WriteUint16(uint16(len(pkg.ErrorMsg)))
 	if err != nil {
 		return fmt.Errorf("failed to write error message length: %w", err)
 	}
+	n += 2
 
 	err = ch.WriteString(pkg.ErrorMsg)
 	if err != nil {
 		return fmt.Errorf("failed to write error message: %w", err)
 	}
+	n += len(pkg.ErrorMsg)
 
-	err = ch.WriteUint8(pkg.ServerLength)
+	err = ch.WriteUint8(uint8(len(pkg.ServerName)))
 	if err != nil {
 		return fmt.Errorf("failed to write servername length: %w", err)
 	}
+	n++
 
 	err = ch.WriteString(pkg.ServerName)
 	if err != nil {
 		return fmt.Errorf("failed to write servername: %w", err)
 	}
+	n += len(pkg.ServerName)
 
-	err = ch.WriteUint8(pkg.ProcLength)
+	err = ch.WriteUint8(uint8(len(pkg.ProcName)))
 	if err != nil {
 		return fmt.Errorf("failed to write procname length: %w", err)
 	}
+	n++
 
 	err = ch.WriteString(pkg.ProcName)
 	if err != nil {
 		return fmt.Errorf("failed to write procname: %w", err)
 	}
+	n += len(pkg.ProcName)
 
 	err = ch.WriteUint16(pkg.LineNr)
 	if err != nil {
 		return fmt.Errorf("failed to write linenr: %w", err)
+	}
+	n += 2
+
+	if n != expectLength {
+		return fmt.Errorf("expected to write %d bytes, wrote %d bytes instead",
+			expectLength, n)
 	}
 
 	return nil
