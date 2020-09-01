52ca40645f31e9ad0ca6f4fba125c0d707949269
Nelo-T. Wallus <nelo@wallus.de>
2020-09-01 13:23:38 +0000

    Export ReturnStatusPackage.ReturnValue

--- libase/tds/packageReturnStatus.go
+++ libase/tds/packageReturnStatus.go
@@ -9,14 +9,13 @@ import "fmt"
 var _ Package = (*ReturnStatusPackage)(nil)
 
 type ReturnStatusPackage struct {
-	returnValue int32
+	ReturnValue int32
 }
 
 func (pkg *ReturnStatusPackage) ReadFrom(ch BytesChannel) error {
 	var err error
 
-	pkg.returnValue, err = ch.Int32()
-	if err != nil {
+	if pkg.ReturnValue, err = ch.Int32(); err != nil {
 		return ErrNotEnoughBytes
 	}
 
@@ -24,13 +23,9 @@ func (pkg *ReturnStatusPackage) ReadFrom(ch BytesChannel) error {
 }
 
 func (pkg ReturnStatusPackage) WriteTo(ch BytesChannel) error {
-	return ch.WriteInt32(pkg.returnValue)
-}
-
-func (pkg ReturnStatusPackage) ReturnValue() int {
-	return int(pkg.returnValue)
+	return ch.WriteInt32(pkg.ReturnValue)
 }
 
 func (pkg ReturnStatusPackage) String() string {
-	return fmt.Sprintf("%T(%d)", pkg, pkg.returnValue)
+	return fmt.Sprintf("%T(%d)", pkg, pkg.ReturnValue)
 }
