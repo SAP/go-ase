4a8e22c92d5b7186f346bb36cd0dd4a4d7bd93ee
Nelo-T. Wallus <nelo@wallus.de>
2020-09-08 12:32:23 +0000

    Implement database/sql.Tx

--- purego/dynamic.go
+++ purego/dynamic.go
@@ -112,8 +112,9 @@ func (stmt *Stmt) allocateOnServer(ctx context.Context) error {
 				stmt.rowFmt = typed
 				return false, nil
 			case *tds.DonePackage:
-				if typed.Status != tds.TDS_DONE_FINAL {
-					return false, fmt.Errorf("DonePackage does not have status TDS_DONE_FINAL set: %s", typed)
+				if typed.Status != tds.TDS_DONE_FINAL && typed.Status != tds.TDS_DONE_INXACT {
+					return false, fmt.Errorf("DonePackage does not have status TDS_DONE_FINAL or TDS_DONE_INXACT set: %s",
+						typed)
 				}
 				return true, nil
 			default:
