977dd7e19813bc510689fbbb7578ccfe37718f0a
Frederic Wilhelm <Frederic.Wilhelm@sap.com>
2020-11-04 12:24:15 +0000

    Minor style- and doc-fixes

--- doc.go
+++ doc.go
@@ -5,44 +5,4 @@
 // Package ase contains code of the go-ase driver for the database/sql
 // package of Go (golang) to provide access to SAP ASE instances.
 //
-// It implements the following database/sql/driver-interfaces:
-//
-// - driver.Conn
-//
-// - driver.ConnPrepareContext
-//
-// - driver.ConnBeginTx
-//
-// - driver.Connector
-//
-// - driver.Driver
-//
-// - driver.DriverContext
-//
-// - driver.ExecerContext
-//
-// - driver.NamedValueChecker
-//
-// - driver.Pinger
-//
-// - driver.QueryerContext
-//
-// - driver.Result
-//
-// - driver.Rows
-//
-// - driver.RowsNextResultSet
-//
-// - driver.RowsColumnTypeDatabaseTypeName
-//
-// - driver.RowsColumnTypeLength
-//
-// - driver.Stmt
-//
-// - driver.StmtExecContext
-//
-// - driver.StmtQueryContext
-//
-// - driver.Tx
-//
 package ase
