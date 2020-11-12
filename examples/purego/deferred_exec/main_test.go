900f15115a4539e456bd483a95c76f3db48677e3
Nelo-T. Wallus <nelo@wallus.de>
2020-11-12 10:54:29 +0000

    Add example on deferred exec

---
+++ examples/purego/deferred_exec/main_test.go
@@ -0,0 +1,28 @@
+// SPDX-FileCopyrightText: 2020 SAP SE
+//
+// SPDX-License-Identifier: Apache-2.0
+
+// +build integration
+
+package main
+
+import "log"
+
+func ExampleDoMain() {
+	if err := DoMain(); err != nil {
+		log.Printf("Failed to execute example: %v", err)
+	}
+	// Output:
+	//
+	// Opening database
+	// configuring errorlog size
+	// doSimple on sql.DB
+	// Creating table 'simple'
+	// Writing a=2147483647, b='a string' to table
+	// Querying values from table
+	// Displaying results of query
+	// | a          | b                              |
+	// | 2147483647 | a string                       |
+	// resetting errorlog size
+	// reset errorlog size
+}
