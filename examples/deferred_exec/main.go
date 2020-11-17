2200c31a1a07da3811c03175a3616b08f7dcb21c
Frederic Wilhelm <Frederic.Wilhelm@sap.com>
2020-11-17 09:30:09 +0000

    Adjust example to (new) go-ase-structure
    `libase` -> `go-dblib`
    `libdsn` -> `dsn`
    
    (Note: `make integration` ran successfully at `go-dblib`-version
    @mrgCommits-branch)

--- examples/deferred_exec/main.go
+++ examples/deferred_exec/main.go
@@ -12,8 +12,8 @@ import (
 	"log"
 	"math"
 
-	"github.com/SAP/go-ase/libase/libdsn"
-	_ "github.com/SAP/go-ase/purego"
+	_ "github.com/SAP/go-ase"
+	"github.com/SAP/go-dblib/dsn"
 )
 
 func main() {
@@ -23,7 +23,7 @@ func main() {
 }
 
 func DoMain() error {
-	dsn, err := libdsn.NewInfoFromEnv("")
+	dsn, err := dsn.NewInfoFromEnv("")
 	if err != nil {
 		return fmt.Errorf("error reading DSN info from env: %w", err)
 	}
