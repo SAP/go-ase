6c084b2e635cff92f7ff1127d93e108673ec690b
Frederic Wilhelm <Frederic.Wilhelm@sap.com>
2022-02-09 17:15:06 +0000

    integration_test.go: Add null-type tests

--- integration_test.go
+++ integration_test.go
@@ -4,6 +4,7 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
+//go:build integration
 // +build integration
 
 package ase
@@ -64,33 +65,67 @@ func TestUint8(t *testing.T) { integration.DoTestUnsignedBigInt(t) }
 func TestUint4(t *testing.T) { integration.DoTestUnsignedInt(t) }
 func TestUint2(t *testing.T) { integration.DoTestUnsignedSmallInt(t) }
 
+// Nullable exact numeric integer
+func TestNullInt8(t *testing.T)  { integration.DoTestNullBigInt(t) }
+func TestNullInt4(t *testing.T)  { integration.DoTestNullInt(t) }
+func TestNullInt2(t *testing.T)  { integration.DoTestNullSmallInt(t) }
+func TestNullInt1(t *testing.T)  { integration.DoTestNullTinyInt(t) }
+func TestNullUint8(t *testing.T) { integration.DoTestNullUnsignedBigInt(t) }
+func TestNullUint4(t *testing.T) { integration.DoTestNullUnsignedInt(t) }
+func TestNullUint2(t *testing.T) { integration.DoTestNullUnsignedSmallInt(t) }
+
 // Exact numeric decimal
 func TestDecimal(t *testing.T)     { integration.DoTestDecimal(t) }
 func TestDecimal10(t *testing.T)   { integration.DoTestDecimal10(t) }
 func TestDecimal380(t *testing.T)  { integration.DoTestDecimal380(t) }
 func TestDecimal3838(t *testing.T) { integration.DoTestDecimal3838(t) }
 
+// Nullable exact numeric decimal
+func TestNullDecimal(t *testing.T) { integration.DoTestNullDecimal(t) }
+
 // Approximate numeric
 func TestFlt8(t *testing.T) { integration.DoTestFloat(t) }
 func TestFlt4(t *testing.T) { integration.DoTestReal(t) }
+
+// Nullable approximate numeric
+func TestNullFlt8(t *testing.T) { integration.DoTestNullFloat(t) }
+func TestNullFlt4(t *testing.T) { integration.DoTestNullReal(t) }
 
 // Money
 func TestMoney(t *testing.T)      { integration.DoTestMoney(t) }
 func TestShortmoney(t *testing.T) { integration.DoTestMoney4(t) }
 
+// Nullable Money
+func TestNullMoney(t *testing.T)      { integration.DoTestNullMoney(t) }
+func TestNullShortmoney(t *testing.T) { integration.DoTestNullMoney4(t) }
+
 // Date and time
 func TestDateN(t *testing.T)         { integration.DoTestDate(t) }
 func TestTimeN(t *testing.T)         { integration.DoTestTime(t) }
+func TestBigTime(t *testing.T)       { integration.DoTestBigTime(t) }
 func TestSmallDateTime(t *testing.T) { integration.DoTestSmallDateTime(t) }
 func TestDateTime(t *testing.T)      { integration.DoTestDateTime(t) }
 func TestBigDateTime(t *testing.T)   { integration.DoTestBigDateTime(t) }
-func TestBigTime(t *testing.T)       { integration.DoTestBigTime(t) }
+
+// Nullable Date and time
+func TestNullDate(t *testing.T)          { integration.DoTestNullDate(t) }
+func TestNullTime(t *testing.T)          { integration.DoTestNullTime(t) }
+func TestNullBigTime(t *testing.T)       { integration.DoTestNullBigTime(t) }
+func TestNullSmallDateTime(t *testing.T) { integration.DoTestNullSmallDateTime(t) }
+func TestNullDateTime(t *testing.T)      { integration.DoTestNullDateTime(t) }
+func TestNullBigDateTime(t *testing.T)   { integration.DoTestNullBigDateTime(t) }
 
 // Character
-func TestVarChar(t *testing.T)  { integration.DoTestVarChar(t) }
 func TestChar(t *testing.T)     { integration.DoTestChar(t) }
 func TestNChar(t *testing.T)    { integration.DoTestNChar(t) }
+func TestVarChar(t *testing.T)  { integration.DoTestVarChar(t) }
 func TestNVarChar(t *testing.T) { integration.DoTestNVarChar(t) }
+
+// Nullable Character
+func TestNullChar(t *testing.T)     { integration.DoTestNullChar(t) }
+func TestNullNChar(t *testing.T)    { integration.DoTestNullNChar(t) }
+func TestNullVarChar(t *testing.T)  { integration.DoTestNullVarChar(t) }
+func TestNullNVarChar(t *testing.T) { integration.DoTestNullNVarChar(t) }
 
 // TODO func TestText(t *testing.T)     { integration.DoTestText(t) }
 // TODO func TestUniChar(t *testing.T)  { integration.DoTestUniChar(t) }
@@ -99,6 +134,10 @@ func TestNVarChar(t *testing.T) { integration.DoTestNVarChar(t) }
 // Binary
 func TestBinary(t *testing.T)    { integration.DoTestBinary(t) }
 func TestVarBinary(t *testing.T) { integration.DoTestVarBinary(t) }
+
+// Nullable Binary
+func TestNullBinary(t *testing.T)    { integration.DoTestNullBinary(t) }
+func TestNullVarBinary(t *testing.T) { integration.DoTestNullVarBinary(t) }
 
 // Bit
 func TestBit(t *testing.T) { integration.DoTestBit(t) }
